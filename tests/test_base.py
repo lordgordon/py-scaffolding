9b65c1beed2a6ac275cef6388cfb317ae9596f69
Nicholas Fiorentini <lordgordon@users.noreply.github.com>
2021-02-11 21:37:39 +0100

    #11 hypotesis example

--- tests/test_base.py
+++ tests/test_base.py
@@ -1,8 +1,15 @@
-"""A very simple test suite."""
+"""A very simple test suite to test the proper setup."""
 
+from hypothesis import given, strategies as st
 from src import __title__
 
 
 def test_working():
     """Just to test everything is running as expected."""
     assert __title__ == "py-scaffolding"
+
+
+@given(st.integers(), st.integers())
+def test_ints_are_commutative(x, y):
+    """Test hypotesis is running."""
+    assert x + y == y + x
