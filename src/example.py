950ecacb685a0a332d2642d657e3d840515effbd
Nicholas Fiorentini <lordgordon@users.noreply.github.com>
2021-02-17 21:54:37 +0100

    #23 doctest execution with pytest and example

---
+++ src/example.py
@@ -0,0 +1,13 @@
+"""An example to show everything is working."""
+
+
+def demo() -> int:
+    """
+    A very simple function to show docstrings with doctests are executed by pytest.
+
+    Use this function like this:
+
+    >>> demo()
+    42
+    """
+    return 42
