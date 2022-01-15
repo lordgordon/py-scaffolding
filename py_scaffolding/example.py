18efa6114ac6769cfeeaef092d3b2c14cc8a5937
Nicholas Fiorentini <lordgordon@users.noreply.github.com>
2022-01-15 16:40:46 +0100

    #53 structlog example usage and testing

--- py_scaffolding/example.py
+++ py_scaffolding/example.py
@@ -1,4 +1,7 @@
 """An example to show everything is working."""
+from py_scaffolding.logging import set_up_logger
+
+LOG = set_up_logger(logger_name=__name__)
 
 
 def demo() -> int:
@@ -11,3 +14,14 @@ def demo() -> int:
     42
     """
     return 42
+
+
+def demo_with_logger() -> None:
+    """
+    To test and demonstrate the use of logger.
+    """
+    LOG.info(
+        "test log message",
+        some_key="some value",
+        another_key=True,
+    )
